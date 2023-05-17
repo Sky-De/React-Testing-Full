37375fc28e8be956a436d2391a70740accdb07f2
SkyDe <sky_de1991@outlook.com>
2023-05-17 21:23:38 +0430

    single test wrap by provider added

--- src/components/modeIcon/ModeIcon.test.tsx
+++ src/components/modeIcon/ModeIcon.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { customRender } from '../../test-utils';
 import ModeIcon from './ModeIcon';
 import user from '@testing-library/user-event';
+import { AppProviders } from '../../context/AppProviders';
 
 
 describe('ModeToggleComponet', ()=> {
@@ -12,7 +13,18 @@ describe('ModeToggleComponet', ()=> {
     expect(iconElement).toHaveClass("bx-moon");
   });
 
+  // wrap only one test with providers
   test('toggles Icon correctly', async () => {
+    user.setup();
+    render(<ModeIcon />, { wrapper: AppProviders});
+    const iconElement = screen.getByRole("img");
+    const divBtn = screen.getByRole("button");
+    await user.click(divBtn);
+    expect(iconElement).toHaveClass("bx-sun");
+  });
+  
+  // wrap all tests with providers using custom render function
+  test('toggles Icon correctly again', async () => {
     user.setup();
     customRender(<ModeIcon />);
     const iconElement = screen.getByRole("img");
